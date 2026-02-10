6d149022ed61c231ec53c3da342ba07a0995ba8a
lolofficial <lolgamer12_yt@proton.me>
2026-02-10 17:08:15 +0100

    Create test-bot.py

---
+++ test-bot.py
@@ -0,0 +1,27 @@
+import discord
+
+# la variabile intents contiene i permessi al bot
+intents = discord.Intents.default()
+# abilita il permesso a leggere i contenuti dei messaggi
+intents.message_content = True
+# crea un bot e passa gli indents
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Abbiamo fatto l\'accesso come {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$ciao'):
+        await message.channel.send("Ciao!")
+    if message.content.startswith('$buongiorno'): 
+        await message.channel.send('Buongiorno!')
+    elif message.content.startswith('$arrivederci'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("INSERISCI TOKEN")
